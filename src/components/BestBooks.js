51ad1035bcafb2c21ce29bcd314eab9fbfa1df8c
Robert Ball <bobball89@gmail.com>
2022-08-30 15:17:04 -0700

    submit and delete functions work, readme update lab12 features complete

--- src/components/BestBooks.js
+++ src/components/BestBooks.js
@@ -2,8 +2,7 @@ import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import axios from 'axios';
 import DisplayBooks from './DisplayBooks.js';
-// import { AccordionCollapse } from 'react-bootstrap';
-import BookFormModal from './components/BookFormModal.js'
+import BookFormModal from './BookFormModal.js'
 
 const booksURL = `${process.env.REACT_APP_SERVER}`;
 
@@ -32,52 +31,13 @@ class BestBooks extends React.Component {
     }
   }
 
-// --------------------- get books refactored to above code ----------------------------
-  // getBooks = () => {
-  //   axios.get(`${booksURL}/books`).then(response => {
-  //     this.setState({
-  //       books: response.data
-  //     });
-  //     if (response.data.length === 0) {
-  //       this.setState({
-  //         noBooks: true
-  //       });
-  //     }
-  //     else this.setState({ noBooks: false })
-  //   }
-  //   )
-  // }
-// --------------------- get books refactored to above code ----------------------------
-
-
-///////// In class demo handle create
-
-//   handleBookCreate = async (bookInfo) => {
-//     console.log(bookInfo);
-//     try {
-//       const response = await axios.post(`${booksURL}, bookInfo`);
-//       const newBook = response.data;
-//       const books = [...this.state.books, newBook];
-//       this.setState({ books });
-//     } catch (error) {
-//       console.log('error is book post: ', error.response);
-//     }
-//   }
-
-
-// ///////// In class demo handle delete
-
 handleBookDelete = async (bookToDelete) =>{
   try {
-    //make axios.delete request
     const response = await axios.delete(`${booksURL}/books/${bookToDelete._id}`);
-    // console.log response.data
     console.log(response.status);
-    // .filter on the front end
     const filteredBooks = this.state.books.filter(book => {
       return book._id !== bookToDelete._id
     })
-    // .setState with filtered books
     this.setState({
       books: filteredBooks
     })
@@ -87,9 +47,6 @@ handleBookDelete = async (bookToDelete) =>{
     console.log(error);
   }
 }
-
-
-////////////
 
   render() {
     console.log(this.state.books);
